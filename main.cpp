11e219b759f9ddaa788b6f3bd024d0e6e065cde1
Ivan <ivan.morozov@list.ru>
2018-11-12 23:26:38 +0300

    Добавил классы User, Session, и тестирование этих классов Так же был реализован паттерн билдер для класса Session

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
-//
-// Created by daniknik on 28.10.18.
-//
-
-#include <iostream>
+//#include <iostream>
+//#include "session_by_name.h"
+//#include "session_director.h"
+//#include "sys.h"
 
 int main() {
+//  Sys *system_table = Sys::get_instance();
+//
+//  system_table->add_user("abcd", "Ivan", 0);
+//  system_table->add_user("qwerty", "Name", 1);
+//  system_table->add_user("thcp", "John", 5);
+//
+//  SessionType type = SBN;
+//  std::string token = "abcd";
+//
+//  SessionByName sba_session;
+//  SessionDirector director(&sba_session);
+//  director.create_session(token, type);
+//
+//  auto ses = sba_session.get_result();
+//
+//  if(sba_session.get_result()->is_connect()) {
+//    std::cout << ses->get_user_token() << " " << ses->get_user_name() << " "
+//              << ses->get_user_permissions();
+//  } else {
+//    std::cout << "Пользователя с такими токеном не существует" << std::endl;
+//  }
+
+
   return 0;
 }
