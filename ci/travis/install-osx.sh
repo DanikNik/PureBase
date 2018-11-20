1ed7de18d3039814dac02e742bcd093b7a0c50f7
DanikNik <sway9009>
2018-11-20 20:46:44 +0300

    ci base

---
+++ ci/travis/install-osx.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+git clone https://github.com/oktal/pistache.git
+git submodule update --init
+cd pistache
+mkdir build
+cd build
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
+make
+sudo make install
+
+cd
+
+git clone https://github.com/abseil/googletest.git
+cd googletest
+mkdir build       # Create a directory to hold the build output.
+cd build
+cmake ..  # Generate native build scripts.
+make
+sudo make install
