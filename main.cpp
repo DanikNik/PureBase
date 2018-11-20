46e86dd343e66f9c02d2d435db9535b4cc93529e
DanikNik <sway9009>
2018-11-20 19:16:31 +0300

    Каркасы ApiEndpoint и Query

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@
    Example of an hello world server
 */
 
+#include "ApiEndpoint/api_endpoint.h"
+
 int main(int argc, char** argv) {
+  Port port(9080);
+  Address addr(Ipv4::any(), port);
+  ApiEndpoint endpoint(addr);
+  endpoint.init(1);
+  endpoint.start();
+  endpoint.shutdown();
   return 0;
 }
