1e3fa920189a35c52c1d48a9fab9502a41d795c7
DanikNik <nikolsky.dan@gmail.com>
2018-12-02 19:37:50 +0300

    Добавил заглушку парсера и начальную логику

--- Server/server.cc
+++ Server/server.cc
@@ -1,10 +1,43 @@
-//
-// Created by daniknik on 02.12.18.
-//
-
 #include "server.h"
-#include "../CreateDBCommand/create_db_command.h"
 
+void Server::client_work(std::shared_ptr <Socket> client) {
+  client->setRcvTimeout(/*sec*/30, /*microsec*/0);
+  while (true)
+    try {
+
+      std::string line = client->recv();
+
+      std::thread producer([&]() {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::unique_lock<std::mutex> lock(m);
+        CreateDBCommand *command = new CreateDBCommand(helper, line);
+        app->setCommand(command);
+        notified = true;
+        cond_var.notify_all();
+      });
+
+      std::thread consumer([&]() {
+        std::unique_lock<std::mutex> lock(m);
+        while (!notified) {
+          cond_var.wait(lock);
+        }
+        app->checkQueue();
+        notified = false;
+      });
+
+      producer.join();
+      consumer.join();
+
+      Sys *system_table = Sys::get_instance();
+
+      client->send("response:  " + system_table->get_user_name(line) + '\n');
+
+    }
+    catch (const std::exception &e) {
+      std::cerr << "exception: " << e.what() << std::endl;
+      return;
+    }
+}
 void Server::start() {
   try {
     Socket s;
@@ -16,29 +49,14 @@ void Server::start() {
     } else {
       std::cerr << "child: " << getpid() << std::endl;
     }
-
-    while (true) {
+    while(true)
+    {
       std::shared_ptr<Socket> client = s.accept();
       client_work(client);
     }
   }
-  catch (const std::exception &e) {
+  catch(const std::exception &e)
+  {
     std::cerr << e.what() << std::endl;
   }
-}
-void Server::client_work(std::shared_ptr<Socket> client) {
-  client->setRcvTimeout(/*sec*/30, /*microsec*/0);
-  while (true)
-    try {
-      std::string line = client->recv();
-      Helper* ret = parser->query(line);
-      CreateDBCommand *create_db_command = new CreateDBCommand(helper, line);
-      app->setCommand(create_db_command);
-      client->send("response: " + line + '\n');
-    }
-    catch (const std::exception &e) {
-      std::cerr << "exception: " << e.what() << std::endl;
-      return;
-    }
-}
-
+};
--- Server/server.h
+++ Server/server.h
@@ -15,16 +15,19 @@
 #include "../socket/socket.hpp"
 #include "../Application/application.h"
 #include "../Helper/helper.h"
+#include "../LessonHelper/lesson_helper.h"
+#include "../DataHelper/Video_Helper/video_helper.h"
 #include "../DataHelper/Document_Helper/document_helper.h"
 #include "../Parser/parser.h"
 #include "../sys/sys.h"
+#include "../CreateDBCommand/create_db_command.h"
 using namespace std;
 
 class Server {
  private:
   int port;
-    Parser *parser;
-  LessonHelper *lesson_helper;
+  Parser *parser;
+  Helper *helper;
   Application *app = new Application();
   std::queue<std::string> produced_nums;
   std::mutex m;
@@ -32,62 +35,12 @@ class Server {
   bool notified = false;
 
  public:
-  Server(): port(8090), app(new Application()), parser(new Parser()), helper(new DocumentHelper()){}
-  Server(int _port): port(_port), app(new Application()), parser(new Parser()), helper(new DocumentHelper()){}
+  Server() : port(8090), app(new Application()), parser(new Parser()), helper(new DocumentHelper()) {}
+  Server(int _port) : port(_port), app(new Application()), parser(new Parser()), helper(new DocumentHelper()) {}
   virtual ~Server() = default;
 
-  void client_work(std::shared_ptr<Socket> client) {
-    client->setRcvTimeout(/*sec*/30, /*microsec*/0);
-    while (true)
-      try {
+  void client_work(std::shared_ptr<Socket> client);
 
-        std::string line = client->recv();
-
-        std::thread producer([&]() {
-          std::this_thread::sleep_for(std::chrono::seconds(1));
-          std::unique_lock<std::mutex> lock(m);
-          CreateDBCommand *command = new CreateDBCommand(lesson_helper, line);
-          app->setCommand(command);
-          notified = true;
-          cond_var.notify_all();
-        });
-
-        std::thread consumer([&]() {
-          std::unique_lock<std::mutex> lock(m);
-          while (!notified) {
-            cond_var.wait(lock);
-          }
-          app->checkQueue();
-          notified = false;
-        });
-
-        producer.join();
-        consumer.join();
-
-        Sys *system_table = Sys::get_instance();
-
-        client->send("response:  " + system_table->get_user_name(line) + '\n');
-
-      }
-      catch (const std::exception &e) {
-        std::cerr << "exception: " << e.what() << std::endl;
-        return;
-      }
-  }
-
-  void start() {
-    try {
-      Socket s;
-      s.createServerSocket(port, 25);
-
-      pid_t pid = fork();
-      if (pid > 0) {
-        std::cerr << "parent: " << getpid() << std::endl;
-      } else {
-        std::cerr << "child: " << getpid() << std::endl;
-      }
-
-
+  void start();
 };
-
 #endif //PUREBASE_SERVER_H
