f9dc615f7a88497cd0e0fa5a2693defca9e9d7d2
DanikNik <sway9009>
2018-12-03 18:40:54 +0300

    updated install-linux.sh for travis ci test commit

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -3,15 +3,14 @@
 sudo apt update
 sudo apt install make
 sudo apt install
-sudo apt install libboost-dev libboost-program-options-dev libgtest-dev
+sudo apt install libboost-dev libboost-program-options-dev
 
-#git clone https://github.com/abseil/googletest.git
-#git submodule update --init
-#cd googletest
-#mkdir build       # Create a directory to hold the build output.
-#cd build
-#cmake ..  # Generate native build scripts.
-#make
-#sudo make install
-#
-#cd ../..
+git clone https://github.com/abseil/googletest.git
+git submodule update --init
+cd googletest
+mkdir build       # Create a directory to hold the build output.
+cd build
+cmake ..  # Generate native build scripts.
+make
+sudo make install
+cd ../..
