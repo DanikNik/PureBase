e35132be19e3126003d1407fbfe8e1c24b59ef93
DanikNik <nikolsky.dan@gmail.com>
2018-12-14 01:41:40 +0300

    basic json-reading client

--- Client/simple_cmd_client.cc
+++ Client/simple_cmd_client.cc
@@ -3,3 +3,20 @@
 //
 
 #include "simple_cmd_client.h"
+void Client::start() {
+  work();
+}
+int Client::work() {
+  std::string path;
+  std::cin >> path;
+  std::ifstream fin(path);
+  std::stringstream ss;
+  ss << fin.rdbuf();
+
+  Socket socket;
+  socket.connect(host, port);
+  socket.send(ss.str());
+  std::cout << socket.recv() << std::endl;
+
+  return 0;
+}
--- Client/simple_cmd_client.h
+++ Client/simple_cmd_client.h
@@ -5,11 +5,23 @@
 #ifndef PUREBASE_SIMPLE_CMD_CLIENT_H
 #define PUREBASE_SIMPLE_CMD_CLIENT_H
 
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include "../socket/socket.hpp"
+
 class Client {
  private:
+  int work();
+  std::string host;
+  int port;
+
  public:
   Client() = default;
+  Client(std::string _host, int _port):host(_host), port(_port){}
  ~Client() = default;
+
+ void start();
 };
 
 #endif //PUREBASE_SIMPLE_CMD_CLIENT_H
