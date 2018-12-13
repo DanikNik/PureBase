65edf847f3141dfce8a5374da9d1e5da0d3a13d0
DanikNik <sway9009>
2018-12-13 18:03:01 +0300

    testing ci with libpqxx

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -2,15 +2,27 @@
 
 sudo apt update
 sudo apt install make
-#sudo apt install libpqxx-4.0v5 libpq-dev
 sudo apt install libboost-all-dev
+sudo apt install postgresql-all
+
+
+git clone https://github.com/jtv/libpqxx
+cd libpqxx/
+mkdir build
+cd build/
+cmake ..
+make -j8
+sudo make install
+cd ../..
+
+
 
 git clone https://github.com/abseil/googletest.git
 git submodule update --init
-cd googletest
+cd googletest/
 mkdir build       # Create a directory to hold the build output.
 cd build
 cmake ..  # Generate native build scripts.
-make
+make -j8
 sudo make install
 cd ../..
