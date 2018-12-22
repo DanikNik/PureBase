f93bfc9f18f4b07f506dcc5c65d38cf8fa14ceab
Ivan <ivan.morozov@list.ru>
2018-12-23 00:57:49 +0300

    add qt interface

---
+++ Client/mainwindow.cc
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() {
+  delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked() {
+    std::cout << "hui";
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    std::cout << "hui";
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    std::cout << "hui";
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    std::cout << "hui";
+}
